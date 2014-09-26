b65540515141853ec9b637379ea1abd4818c337c
Matt Vargish <matt_vargish@teamstudio.com>
2014-09-26 16:24:44 -0400

    Add basic pull test, minor fixes to pull

--- tests/adapter.tests.js
+++ tests/adapter.tests.js
@@ -1,6 +1,7 @@
 
 var should = require('chai').should();
 var _ = require('lodash');
+var Q = require('q');
 var LowlaAdapter = require('../lib/adapter.js').LowlaAdapter;
 
 describe('LowlaAdapter', function() {
@@ -54,7 +55,7 @@ describe('LowlaAdapter', function() {
     it('should create the document in MongoDB', function(done) {
       lowlaDb.pushWithPayload(testDocPayload)
         .then(function() {
-          testColl.find().toArray(function(err, docs) {
+              testColl.find().toArray(function(err, docs) {
             should.not.exist(err);
             should.exist(docs);
             docs.length.should.equal(1);
@@ -111,4 +112,114 @@ describe('LowlaAdapter', function() {
         .catch(done);
     });
   })
+
+    describe('Pull', function() {
+
+        it('should return a test document', function () {
+
+            var newDoc = {_id:'1234', _version:1, a:1, b:2 }
+            var deferred = Q.defer();
+            testColl.insert(newDoc, function(err, result){
+                if(err){
+                    deferred.reject(err);
+                }
+                deferred.resolve(result);
+            })
+
+            return deferred.promise.then(function(){
+                return lowlaDb.pullWithPayload(null)
+                    .then(function (result) {
+                        should.exist(result);
+                        result.should.have.length.of(2);
+                        result[0].id.should.equal('lowladbtest.TestCollection$1234');
+                        result[0].version.should.equal(1);
+                        result[0].clientNs.should.equal('lowladbtest.TestCollection');
+                        result[1].a.should.equal(1);
+                        result[1].b.should.equal(2);
+                        result[1]._version.should.equal(1);
+                    })
+            });
+
+        });
+
+        it('should return several test documents', function () {
+            var docs=[];
+            for(i=0; i<=9; i++){
+                docs.push({_id:'1234'+i, _version:i, a:1000+i, b:2000+i })
+            }
+
+            var deferred = Q.defer();
+            testColl.insert(docs, function(err, result){
+                if(err){
+                    deferred.reject(err);
+                }
+                deferred.resolve(result);
+            });
+
+            return deferred.promise.then(function(){
+                return lowlaDb.pullWithPayload(null)
+                    .then(function (result) {
+                        should.exist(result);
+                        result.should.have.length.of(20);
+                        var j=0;
+                        for(i=0; i<20; i+=2) {
+                            result[i].id.should.equal('lowladbtest.TestCollection$1234'+j);
+                            result[i].version.should.equal(j);
+                            result[i].clientNs.should.equal('lowladbtest.TestCollection');
+                            result[i+1].a.should.equal(1000+j);
+                            result[i+1].b.should.equal(2000+j);
+                            result[i+1]._version.should.equal(j);
+                            j++;
+                        }
+
+                    });
+            });
+
+        });
+
+        it('should return requested documents', function () {
+            var docs=[];
+            var payload = {};
+            payload.ids = [];
+            for(i=0; i<=9; i++){
+                docs.push({_id:'1234'+i, _version:i, a:1000+i, b:2000+i, testKey:i})
+                if(0==i % 2){
+                    payload.ids.push('lowladbtest.TestCollection$1234'+i);
+                }
+            }
+
+            var deferred = Q.defer();
+            testColl.insert(docs, function(err, result){
+                if(err){
+                    deferred.reject(err);
+                }
+                deferred.resolve(result);
+            });
+
+            return deferred.promise.then(function(){
+                return lowlaDb.pullWithPayload(payload)
+                    .then(function (result) {
+                        should.exist(result);
+                        result.should.have.length.of(10);
+                        var testKey;
+                        for(i=0; i<10; i+=2) {
+                            testKey = result[i+1].testKey;  //version
+                            result[i].id.should.equal('lowladbtest.TestCollection$1234'+testKey);
+                            payload.ids.should.include(result[i].id);   //was it requested?
+                            result[i].version.should.equal(testKey);
+                            result[i].clientNs.should.equal('lowladbtest.TestCollection');
+                            result[i+1].a.should.equal(1000+testKey);
+                            result[i+1].b.should.equal(2000+testKey);
+                            result[i+1]._version.should.equal(testKey);
+                        }
+
+                    });
+            });
+
+        });
+
+
+    });
+
+
 });
