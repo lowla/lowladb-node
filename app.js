af420c709fdf0de3fa67b762ba65c8c77e5402b9
Matt Vargish <matt_vargish@teamstudio.com>
2014-11-14 16:05:00 -0500

    Added bunyan as an example logger (at the app.js level), may not be the final choice.

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
-var logger = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+//Create a logger to pass to Lowla.  This example uses bunyan, but any standard logger will do so long as it implements the required calls. //TODO doc this requirement
+var bunyan = require('bunyan');
+var PrettyStream = require('bunyan-prettystream');  //TODO bunyan-prettystream is not recommended for production use - but great for webstorm dev.
+var prettyStdOut = new PrettyStream();
+prettyStdOut.pipe(process.stdout);
+var logConfig = { name: 'LowlaDb',streams: [
+  { level: 'debug', stream: prettyStdOut },
+  { level: 'debug', path: 'LowLa.log' }
+]};
+var logger = bunyan.createLogger(logConfig);
+logger.stream = (function(_thisLogger){
+  return {
+    write:function(message, encoding){
+      _thisLogger.trace(message.slice(0, -1));
+    }
+  }
+})(logger);
+logger.log = logger.debug;
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,6 +31,7 @@ var users = require('./routes/users');
 var lowlaConfig = {
   syncUrl: 'mongodb://127.0.0.1/lowlasync',
   mongoUrl: 'mongodb://127.0.0.1/lowladb'
+ , logger: logger
 };
 
 var LowlaSync = require('./lib/sync.js').LowlaSyncer;
@@ -27,7 +48,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(favicon());
-app.use(logger('dev'));
+app.use(morgan({stream: logger.stream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
@@ -65,23 +86,25 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
+
+
